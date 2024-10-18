668afd9607177e6a94c4afde96e3194e692bd2e8
Navarr <navarr.barnier@aeroflowinc.com>
2024-10-18 10:10:22 -0400

    Lay Groundwork for Twitch messages

--- app/modules/twitch/event/ChatMessageEvent.ts
+++ app/modules/twitch/event/ChatMessageEvent.ts
@@ -2,13 +2,27 @@ import {CommentEvent as BaseChatMessageEvent} from "../../../event/CommentEvent"
 import {ContainsUsername, CouldBeAction, CouldBeAnnouncement} from "../../../types/GenericComponents";
 import {Badge, ContainsBadges} from "../../../types/Badges";
 
-class ChatMessageEvent implements BaseChatMessageEvent, ContainsUsername, ContainsBadges, CouldBeAction, CouldBeAnnouncement {
+export class ChatMessageEvent extends BaseChatMessageEvent implements ContainsUsername, ContainsBadges, CouldBeAction, CouldBeAnnouncement {
     isAnnouncement: boolean;
     isAction: boolean;
     badges: Badge[];
     username: string;
     eventType: string;
-    displayName: string;
-    messageHtml: string;
-    messageText: string;
+
+    /* TODO
+     - Highlights (power up fx, channel points highlight)
+     - Is User Intro?
+     - Profile Image
+     - Who's chat?
+     - Gigantified Emotes
+     - Parent Message
+     */
+
+    constructor(username: string, displayName: string, messageText: string, messageHtml: string, isAction: boolean = false, badges: Badge[] = [], isAnnouncement: boolean = false) {
+        super(displayName, messageHtml, messageText);
+        this.username = username;
+        this.isAction = isAction;
+        this.isAnnouncement = isAnnouncement;
+        this.badges = badges;
+    }
 }
--- app/modules/twitch/module.ts
+++ app/modules/twitch/module.ts
@@ -1,8 +1,18 @@
 import {Module} from "../../framework/Module";
 import {moduleRegistry} from "../../framework/ModuleRegistry";
+import {badgeManager} from "./helper/BadgeManager";
+import {ApiClient} from "@twurple/api";
+import {logger} from "../../framework/Logger";
+import {EventSubWsListener} from "@twurple/eventsub-ws";
+import {ImageBadge} from "../../types/Badges";
+import type {EventSubChannelChatMessageEvent} from "@twurple/eventsub-base/lib/events/EventSubChannelChatMessageEvent";
+import {eventHandler} from "../../framework/EventHandler";
+import {ChatMessageEvent} from "./event/ChatMessageEvent";
+import {TwitchMessageConverter} from "./helper/TwitchMessageConverter";
 
 class TwitchInitializer extends Module {
     public static MODULE_NAME: string = "twitch";
+    private apiClient: ApiClient;
 
     public static getModuleName(): string {
         return TwitchInitializer.MODULE_NAME;
@@ -12,6 +22,44 @@ class TwitchInitializer extends Module {
     }
 
     shutdown(): void {
+    }
+
+    public async initializeConnection(channelId: string) {
+        if (this.apiClient === undefined) {
+            logger.error('Could not initialize Twitch connection, no ApiClient defined', {'channelId': channelId});
+            return;
+        }
+        badgeManager.loadChannelTwitchBadges(this.apiClient, channelId);
+
+        const messageConverter = new TwitchMessageConverter(channelId, this.apiClient);
+        const twitchListener = new EventSubWsListener({apiClient: this.apiClient});
+        twitchListener.start();
+
+        twitchListener.onChannelChatMessage(channelId, channelId, (event: EventSubChannelChatMessageEvent) => {
+            if (event.rewardId !== null) {
+                // Don't show rewards - those should be handled separately.
+                return;
+            }
+
+            const badges = this.getBadgesFromEvent(event);
+
+            eventHandler.submitEvent(new ChatMessageEvent(
+                event.chatterName,
+                event.chatterDisplayName,
+                event.messageText,
+                messageConverter.convertMessage(event.messageParts),
+                event.messageText.startsWith("\u0001ACTION"),
+                badges
+            ))
+        });
+    }
+
+    private getBadgesFromEvent(event: EventSubChannelChatMessageEvent): ImageBadge[] {
+        const badges: ImageBadge[] = [];
+        for(let badgeId in event.badges) {
+            badges.push(badgeManager.getBadge(badgeId, event.badges[badgeId]));
+        }
+        return badges;
     }
 }
 
